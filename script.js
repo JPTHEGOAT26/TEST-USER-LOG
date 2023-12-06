ad6e217b80d2c9b98cc4f8e19d264cb2d71b1229
JPTHEGOAT26 <152308493+JPTHEGOAT26@users.noreply.github.com>
2023-12-06 15:30:24 -0500

    Update script.js

--- script.js
+++ script.js
@@ -1,13 +1,29 @@
+// Function to handle sign-in
 function onSignIn(googleUser) {
   var profile = googleUser.getBasicProfile();
-  console.log('Name: ' + profile.getName());
-  console.log('Image URL: ' + profile.getImageUrl());
-  console.log('Email: ' + profile.getEmail());
+  var userInfo = document.getElementById('userInfo');
+
+  // Clear previous content
+  userInfo.innerHTML = '';
+
+  // Create list items for user information
+  var nameItem = document.createElement('li');
+  nameItem.textContent = 'Name: ' + profile.getName();
+  userInfo.appendChild(nameItem);
+
+  var imageItem = document.createElement('li');
+  imageItem.textContent = 'Image URL: ' + profile.getImageUrl();
+  userInfo.appendChild(imageItem);
+
+  var emailItem = document.createElement('li');
+  emailItem.textContent = 'Email: ' + profile.getEmail();
+  userInfo.appendChild(emailItem);
 }
 
+// Function to handle sign-out
 function signOut() {
-    var auth2 = gapi.auth2.getAuthInstance();
-    auth2.signOut().then(function () {
-      console.log('User signed out.');
-    });
-  }
+  var auth2 = gapi.auth2.getAuthInstance();
+  auth2.signOut().then(function () {
+    console.log('User signed out.');
+  });
+}
