9f9acbe34bbcd2805757eae3c12cc88fb3c6a087
JPTHEGOAT26 <152308493+JPTHEGOAT26@users.noreply.github.com>
2023-12-06 15:01:43 -0500

    Delete script.js

--- script.js
+++
@@ -1,8 +0,0 @@
-function onSignIn(googleUser) {
-  var profile = googleUser.getBasicProfile();
-  var userName = profile.getName();
-
-  document.getElementById('googleSignInBtn').style.display = 'none';
-  document.getElementById('userBox').style.display = 'block';
-  document.getElementById('userName').innerText = userName;
-}
